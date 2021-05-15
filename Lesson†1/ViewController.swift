c068fb7c286d00ab291b2483f7a3b559e8e470ee
Sergey Kalinin <piolersa85@mail.ru>
2021-05-15 23:39:22 +0200

    Setup  Launch Screen storyboard

--- Lesson†1/ViewController.swift
+++ Lesson†1/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        
+        view.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
     }
 
 
